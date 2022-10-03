31a158160b361072db6afb2f38a0f7ec6922a8b8
aidatorajiro <kawarusosu@zoho.com>
2022-10-04 05:38:37 +1100

    journalmanage: refactor

--- src/journalmanage.rs
+++ src/journalmanage.rs
@@ -2,7 +2,7 @@ pub mod systems {
     use bevy::{prelude::*};
     use crate::{typedef::{event::*, component::*, resource::GameState}, utils::utils::*};
 
-    use super::inner::add_entry;
+    use super::inner::{add_entry, add_fragment};
 
     pub fn handle_add_journal(
         mut events: EventReader<AddToFragments>,
@@ -19,20 +19,14 @@ pub mod systems {
                 ev.contents
                 .iter()
                 .map(|x| {
-                    let entid = commands.spawn().insert(Fragment { timestamp: ts, contents: x.clone(), neighbor_graph_index: None, history_graph_index: None }).id();
-                    let mut qmut = fragment_query.get_mut(entid).unwrap();
-                    let neighbor_graph_index = global.neighbor_graph.add_node(entid);
-                    qmut.neighbor_graph_index = Some(neighbor_graph_index);
-                    let history_graph_index = global.history_graph.add_node(entid);
-                    qmut.history_graph_index = Some(history_graph_index);
-                    entid
+                    add_fragment(&mut commands, ts, x, &mut fragment_query, &mut global)
                 })
                 .collect();
 
             if let Some(x) = ev.entry {
-                add_entry(&mut commands, [entitylist.get(x).unwrap().entities.clone(), ents].concat(), ts, &mut global, &fragment_query_r);
+                add_entry(&mut commands, &[entitylist.get(x).unwrap().entities.clone(), ents].concat(), ts, &mut global, &fragment_query_r);
             } else {
-                add_entry(&mut commands, ents, ts, &mut global, &fragment_query_r);
+                add_entry(&mut commands, &ents, ts, &mut global, &fragment_query_r);
             };
         }
     }
@@ -42,13 +36,34 @@ mod inner {
     use bevy::prelude::*;
     use crate::typedef::{resource::*, component::*};
 
+    pub fn add_fragment (
+        commands: &mut Commands,
+        ts: u64,
+        fragment_contents: &FragmentContents,
+        fragment_query: &mut Query<&mut Fragment>,
+        global: &mut ResMut<GameState>
+    ) -> Entity {
+        let entid = commands.spawn().insert(Fragment {
+            timestamp: ts,
+            contents: fragment_contents.clone(),
+            neighbor_graph_index: None,
+            history_graph_index: None
+        }).id();
+        let mut qmut = fragment_query.get_mut(entid).unwrap();
+        let neighbor_graph_index = global.neighbor_graph.add_node(entid);
+        qmut.neighbor_graph_index = Some(neighbor_graph_index);
+        let history_graph_index = global.history_graph.add_node(entid);
+        qmut.history_graph_index = Some(history_graph_index);
+        entid
+    }
+
     pub fn add_entry(
         commands: &mut Commands,
-        entities: Vec<Entity>,
+        entities: &Vec<Entity>,
         ts: u64,
         global: &mut ResMut<GameState>,
         fragment_query: &Query<&Fragment>
-    ) {
+    ) -> Entity {
         let id_entry = commands.spawn().insert(Entry {}).insert(EntityList { timestamp: ts, entities: entities.clone() }).id();
 
         if let Some(mut id_from) = entities.get(0) {
@@ -59,5 +74,7 @@ mod inner {
                 id_from = id_to;
             }
         }
+
+        id_entry
     }
 }
