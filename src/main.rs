d84252ae31ddc482168fa3745295b4cbd1780d29
aidatorajiro <kawarusosu@zoho.com>
2022-10-04 07:26:54 +1100

    main: button layout, change bevy_render to bevy::render

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,24 @@ use assets::assets::RawDataLoader;
 
 use bevy::app::AppExit;
 use bevy::window::WindowClosed;
+use bevy::winit::WinitSettings;
 use constants::constants::*;
 use journalmanage::systems::*;
 use subwindow::systems::*;
+use typedef::component::*;
 use typedef::event::*;
 use typedef::resource::*;
 
 use bevy::{prelude::*, render::RenderApp};
 use bevy_egui::EguiPlugin;
-use bevy_render::{RenderStage};
+use bevy::render::{RenderStage};
 
 /// Main function
 fn main() {
     let mut app = App::new();
     
     app.init_resource::<GameState>()
+        .insert_resource(WinitSettings::desktop_app())
         .add_event::<AddFragments>()
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
@@ -51,30 +54,44 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // 2d camera
     commands.spawn_bundle(Camera2dBundle::default());
 
+    commands.spawn()
+        .insert(SubWindow { initialized: false, window_id: None })
+        .insert(MemoField { textarea: "Hello".to_string() });
+
     commands
-        .spawn_bundle(ButtonBundle {
+        .spawn_bundle(NodeBundle {
             style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                // center button
-                margin: UiRect::all(Val::Auto),
-                // horizontally center child text
-                justify_content: JustifyContent::Center,
-                // vertically center child text
-                align_items: AlignItems::Center,
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::SpaceBetween,
                 ..default()
             },
-            color: NORMAL_BUTTON.into(),
+            color: Color::NONE.into(),
             ..default()
         })
         .with_children(|parent| {
-            parent.spawn_bundle(TextBundle::from_section(
-                "Button",
-                TextStyle {
-                    font: asset_server.load("NotoSansJP-Bold.otf"),
-                    font_size: 40.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            ));
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Percent(50.0), Val::Percent(50.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle::from_section(
+                        "Button",
+                        TextStyle {
+                            font: asset_server.load("NotoSansJP-Bold.otf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
         });
 }
 
