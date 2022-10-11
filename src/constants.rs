2a9b07bda99c1c32271d744e28932a39971735e7
aidatorajiro <kawarusosu@zoho.com>
2022-10-11 15:35:00 +1100

    add background color config

--- src/constants.rs
+++ src/constants.rs
@@ -11,4 +11,5 @@ pub mod style {
     pub const TOPBTN_HOVER: Color = Color::rgb(15.0/256.0, 74.0/256.0, 175.0/256.0);
     pub const TOPBTN_PRESSED: Color = Color::rgb(13.0/256.0, 181.0/256.0, 91.0/256.0);
     pub const TOPBTN_TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+    pub const ICON_BACKGROUND_COLOR: Color = Color::rgb(145.0/256.0, 242.0/256.0, 200.0/256.0);
 }
