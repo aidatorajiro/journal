1a18203a27dfe2dc71372f80b164ba965bedd533
aidatorajiro <kawarusosu@zoho.com>
2022-10-30 00:57:38 +1100

    move save/load systems to journalmanage

--- src/journalmanage.rs
+++ src/journalmanage.rs
@@ -1,8 +1,10 @@
 pub mod systems {
     //! Event and Data management for Journal data structure - systems
 
-    use bevy::{prelude::*};
-    use crate::{typedef::{event::*, resource::GameGraph, resource::FragmentClone}, utils::utils::*};
+    use std::{path::Path, fs::{self, File}, io::Write};
+
+    use bevy::{prelude::*, reflect::TypeRegistry, tasks::IoTaskPool};
+    use crate::{typedef::{event::*, resource::GameGraph, resource::{FragmentClone, StartupManagement}, state::AppState, component::{GameGraphDummy, FragmentContents, Fragment, EntityList, Entry, Tag, TagEvent, TagEventAction}}, utils::utils::*, constants::style::STATE_FILE, journalmanage::inner::{convert_encoded_to_graph, convert_graph_to_encoded}};
 
     use super::inner::{add_entry, add_fragment};
 
@@ -42,13 +44,165 @@ pub mod systems {
             ev_done.send(SyncFragmentsDone { entry_id: e });
         }
     }
+
+    /// Load scene
+    pub fn load_scene_system(world: &mut World) {
+        println!("Checking ron file...");
+        if Path::new(STATE_FILE).exists() { // TODO: maybe use IoTaskPool?
+            println!("Loading ron file...");
+            fs::copy(Path::new(STATE_FILE), Path::new(STATE_FILE).with_extension("ron.".to_string() + &create_timestamp().to_string())).unwrap();
+            let asset_server = world.resource_mut::<AssetServer>();
+            let handle: Handle<DynamicScene> = asset_server.load(Path::new("..").join(STATE_FILE));
+            //let mut spawner =  SceneSpawner::default();
+            //let res = spawner.spawn_sync(world, handle);
+            //println!("{:?}", res);
+            let id = world.spawn().insert_bundle(DynamicSceneBundle {
+                scene: handle,
+                ..default()
+            }).id();
+            println!("Root entity id: {:?}", id);
+        } else {
+            let id = world.spawn().id();
+            println!("Root entity id: {:?}", id);
+            
+            let mut startup = world.resource_mut::<StartupManagement>();
+            startup.state_file_nonexistent = true;
+
+            let mut stat = world.resource_mut::<State<AppState>>();
+            stat.overwrite_set(AppState::TopPage).unwrap();
+        }
+        let mut startup = world.resource_mut::<StartupManagement>();
+        startup.state_file_checked = true;
+    }
+
+    /// Load scene, additional steps for the graph
+    pub fn load_graph_system(
+        mut stat: ResMut<State<AppState>>,
+        mut r: ResMut<GameGraph>,
+        mut startup: ResMut<StartupManagement>,
+        mut commands: Commands,
+        q: Query<(Entity, &GameGraphDummy)>,
+        q_test: Query<Entity, Without<GameGraphDummy>>,
+    ) {
+        let (e, d) = match q.get_single() {Ok(x) => x, _ => return};
+
+        println!("Loading dummy state...");
+
+        let (neighbor_graph, neighbor_graph_ids) = convert_encoded_to_graph(d.neighbor_graph.clone());
+        let (history_graph, history_graph_ids) = convert_encoded_to_graph(d.history_graph.clone());
+        
+        *r = GameGraph {
+            neighbor_graph,
+            neighbor_graph_ids,
+            history_graph,
+            history_graph_ids
+        };
+
+        commands.entity(e).despawn();
+
+        startup.load_graph_done = true;
+
+        println!("{:?}", q_test.iter().collect::<Vec<_>>());
+        
+        stat.overwrite_set(AppState::TopPage).unwrap();
+    }
+
+    /// Save scene
+    pub fn save_scene_system(world: &mut World) {
+
+        if world.resource::<State<AppState>>().current().clone() == AppState::LoadSaveData { return; }
+
+        if !world.is_resource_changed::<GameGraph>() { return; }
+
+        println!("Saving state...");
+
+        // Delete entities with no components
+
+        let parent_id = world.component_id::<Parent>().unwrap();
+
+        let mut q = world.query::<Entity>();
+        let empty_entities = q.iter(world).filter(|x| {
+            let cs = world.inspect_entity(x.clone());
+            if cs.len() == 0 || (cs.len() == 1 && cs.get(0).unwrap().id() == parent_id) {
+                return true
+            } else {
+                return false
+            }
+        }).collect::<Vec<_>>();
+        println!("Found {} empty entity. Deleting...", empty_entities.len());
+        for e in empty_entities {
+            world.despawn(e);
+        }
+        
+        /*
+
+        for archetype in world.archetypes().iter()
+        {
+            if archetype.entities().contains(entity) { return Some(archetype.components()) }
+        }*/
+
+        // Spawn dummy graph
+
+        let graph = world.get_resource::<GameGraph>().unwrap();
+
+        let dummy = GameGraphDummy {
+            neighbor_graph: convert_graph_to_encoded(graph.neighbor_graph.clone()),
+            history_graph: convert_graph_to_encoded(graph.history_graph.clone()),
+        };
+
+        world.spawn().insert(dummy);
+
+        // Register types for saving
+
+        let type_registry = TypeRegistry::default();
+        type_registry.write().register::<Entity>();
+        type_registry.write().register_type_data::<Entity, ReflectSerialize>();
+        type_registry.write().register_type_data::<Entity, ReflectDeserialize>();
+        type_registry.write().register::<FragmentContents>();
+        type_registry.write().register_type_data::<FragmentContents, ReflectSerialize>();
+        type_registry.write().register_type_data::<FragmentContents, ReflectDeserialize>();
+        type_registry.write().register::<String>();
+        type_registry.write().register_type_data::<String, ReflectSerialize>();
+        type_registry.write().register_type_data::<String, ReflectDeserialize>();
+        type_registry.write().register::<Fragment>();
+        type_registry.write().register::<EntityList>();
+        type_registry.write().register::<Entry>();
+        type_registry.write().register::<Tag>();
+        type_registry.write().register::<TagEvent>();
+        type_registry.write().register::<TagEventAction>();
+        type_registry.write().register::<GameGraphDummy>();
+
+        // generate serialized data
+
+        let scene = DynamicScene::from_world(&world, &type_registry);
+        let serialized_scene = match scene.serialize_ron(&type_registry) {Ok(x) => x, Err(x) => {println!("{:?}", x); return}};
+
+        println!("Success! Cleaning...");
+
+        let mut q = world.query::<(Entity, &GameGraphDummy)>();
+        let result = world.despawn(q.single(world).0);
+        
+        #[cfg(not(target_arch = "wasm32"))]
+        IoTaskPool::get()
+            .spawn(async move {
+                File::create(STATE_FILE)
+                    .and_then(|mut file| file.write(serialized_scene.as_bytes()))
+                    .expect("Error while saving state!");
+            })
+            .detach();
+        
+        #[cfg(target_arch = "wasm32")]
+        todo!("TODO: put here localStorage or communication with server or something");
+    }
 }
 
 mod inner {
     //! Event and Data management for Journal data structure - utility functions for fragment data/graph
 
-    use bevy::prelude::*;
+    use bevy::{prelude::*, utils::HashMap};
+    use petgraph::{Graph, graph::NodeIndex};
     use crate::typedef::{resource::*, component::*};
+    use core::hash::Hash;
 
     pub fn add_fragment (
         commands: &mut Commands,
@@ -99,5 +253,50 @@ mod inner {
         }
 
         id_entry
+    }
+
+    /// Construct graph and "weight to index" map from EncodedGraph
+    pub fn convert_encoded_to_graph<A: Hash + Eq + Clone, B> (eg: EncodedGraph<A, B>) -> (Graph<A, B>, HashMap<A, NodeIndex>) {
+
+        let mut neighbor_graph: Graph<A, B> = Graph::new();
+        let mut neighbor_graph_ids: HashMap<A, NodeIndex> = HashMap::new();
+        let mut tmp: HashMap<usize, NodeIndex> = HashMap::new();
+
+        let mut i = 0;
+        for ent_neighbor in eg.0 {
+            let node_id = neighbor_graph.add_node(ent_neighbor.clone());
+            neighbor_graph_ids.insert(ent_neighbor, node_id);
+            tmp.insert(i, node_id);
+            i += 1;
+        }
+
+        for (id_1, id_2, edge) in eg.1 {
+            neighbor_graph.add_edge(*tmp.get(&id_1).unwrap(), *tmp.get(&id_2).unwrap(), edge);
+        }
+
+        (neighbor_graph, neighbor_graph_ids)
+    }
+
+    /// Construct graph and "weight to index" map from EncodedGraph
+    pub fn convert_graph_to_encoded<A: Hash + Eq + Clone, B: Clone> (g: Graph<A, B>) -> EncodedGraph<A, B> {
+        let mut tmp: HashMap<NodeIndex, usize> = HashMap::new();
+        let mut vec_a: Vec<A> = Vec::new();
+        let mut counter = 0;
+
+        for ni in g.node_indices() {
+            let a = g.node_weight(ni).unwrap().clone();
+            vec_a.push(a);
+            tmp.insert(ni, counter);
+            counter += 1;
+        }
+
+        let mut vec_b: Vec<(usize, usize, B)> = Vec::new();
+        for ei in g.edge_indices() {
+            let b = g.edge_weight(ei).unwrap().clone();
+            let nodes = g.edge_endpoints(ei).unwrap();
+            vec_b.push((*tmp.get(&nodes.0).unwrap(), *tmp.get(&nodes.1).unwrap(), b));
+        }
+
+        (vec_a, vec_b)
     }
 }
