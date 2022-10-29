1aab190f094b6a4d5d4440607debb984c346bf16
aidatorajiro <kawarusosu@zoho.com>
2022-10-30 03:25:02 +1100

    constant: move STATE_FILE to save module; add colors

--- src/constants.rs
+++ src/constants.rs
@@ -16,5 +16,12 @@ pub mod style {
     pub const NEWPAGE_HOVER: Color = Color::rgb(195.0/256.0, 232.0/256.0, 46.0/256.0);
     pub const NEWPAGE_CLICK: Color = Color::rgb(230.0/256.0, 152.0/256.0, 44.0/256.0);
 
+    pub const EXPLORE_CUBE_CLICKED: Color = Color::rgb(195.0/256.0, 232.0/256.0, 46.0/256.0);
+    pub const EXPLORE_CUBE_HOVERED: Color = Color::rgb(216.0/256.0, 114.0/256.0, 194.0/256.0);
+    pub const EXPLORE_CUBE_NONE: Color = Color::rgb(3.0/256.0, 30.0/256.0, 7.0/256.0);
+    pub const EXPLORE_CUBE_SELECTED: Color = Color::rgb(138.0/256.0, 206.0/256.0, 153.0/256.0);
+}
+
+pub mod save {
     pub const STATE_FILE: &str = "state.scn.ron";
 }
